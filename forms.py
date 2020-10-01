a58e8b65f7ebb1d3590832a0ac3e9745d5665842
Tan Kai Yung <kaiyungtan2020@gmail.com>
2020-10-01 16:58:35 +0200

    upload images for route predict

--- forms.py
+++ forms.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired 
 
 class LoginForm(FlaskForm):
-	username = StringField('Username', 
-							validators=[DataRequired()])
-	password = PasswordField('Password',
-							validators=[DataRequired()])
+	username = StringField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
 	submit = SubmitField('Login')
 
 
+class PredictForm(FlaskForm):
+	month = IntegerField('Month', validators=[DataRequired()])
+	customer_visiting_website = IntegerField('Customer visiting website',validators=[DataRequired()])
+	seller_avaible =  IntegerField('Seller avaible',validators=[DataRequired()])
+
+	submit = SubmitField('Predict')
  
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import LoginForm
+from forms import LoginForm, PredictForm
+from random import randint
 
 app = Flask(__name__)
 
@@ -21,6 +22,17 @@ def login():
 		flash(f'Login success for {form.username.data} with password length of: {len(form.password.data)}!' ,'success')
 		return redirect(url_for('home'))
 	return render_template('login.html', title='Login', form=form)
+
+@app.route("/predict" ,methods=['GET','POST'])
+def predict():
+	form = PredictForm()
+	value = randint(2000,5000) 
+	if form.validate_on_submit():
+		flash(f'Prediction: {value}' ,'success')
+		return redirect(url_for('home'))
+	return render_template('predict.html', title='Predict', form=form)
+
+
 
 
 if __name__ == '__main__':
